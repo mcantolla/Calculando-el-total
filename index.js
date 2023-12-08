2b061f334d753b41c9be41cace60a98a8906d4ef
Manuel Cantolla <mcantolla81@gmail.com>
2023-12-08 14:25:18 -0300

    First and final commit
    All done

---
+++ index.js
@@ -0,0 +1,32 @@
+// DOM Eelements
+const precioSpan = document.querySelector(".precio-inicial")
+const cantidadSpan = document.querySelector(".cantidad")
+const totalSpan = document.querySelector(".valor-total")
+
+//Variables
+let precio = 400000
+let cantidad = 0
+let total = 0
+
+precioSpan.innerHTML = precio
+cantidadSpan.innerHTML = cantidad
+totalSpan.innerHTML = total
+
+//Functions
+function add(){
+    cantidadSpan.innerHTML = cantidad + 1
+    cantidad = parseInt ((cantidadSpan.innerHTML))
+    total = cantidad * precio
+    totalSpan.innerHTML = total
+}
+
+function substract(){
+    if (totalSpan.innerHTML <= 0){
+        alert("No puede llevar menos de 0")
+        return
+    }
+    cantidadSpan.innerHTML = cantidad - 1
+    cantidad = parseInt ((cantidadSpan.innerHTML))
+    total = cantidad * precio
+    totalSpan.innerHTML = total
+}
